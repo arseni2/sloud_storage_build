9914adaef233c7d3375eefc5e05ee7e2b039ae3f
arseni2 <arc-37@mail.ru>
2023-07-07 21:53:44 +0300

    change url backend api

--- components/FileCard/index.tsx
+++ components/FileCard/index.tsx
@@ -16,7 +16,7 @@ export const FileCard: React.FC<FileCardProps> = ({
 }) => {
   const ext = getExtensionFromFileName(filename);
   const imageUrl =
-    ext && isImage(ext) ? "http://localhost:7777/uploads/" + filename : "";
+    ext && isImage(ext) ? "https://cloudserver-mync.onrender.com//uploads/" + filename : "";
 
   const color = getColorByExtension(ext);
   const classColor = styles[color];
